48af68151d01947bcc6f9faa253d1a33378761b5
Mikaela Grundin <me@mikaelagrundin.com>
2015-05-28 11:22:53 +0200

    Added looping to the slider

--- js/slider.js
+++ js/slider.js
@@ -1,23 +1,35 @@
-var slider = document.querySelectorAll('.img-slider'),
-    images = document.querySelectorAll('.img-item'),
-    imageWidth = images[0].clientWidth;
+var slider = document.getElementsByClassName('img-slider'),
+    images = document.getElementsByClassName('img-item'),
+    imageWidth = images[0].clientWidth,
+    start = images[0].cloneNode(true);
 
-    slider[0].style.width = imageWidth * images.length + "px";
+    //Clones the first image to the end of the slider to make for a smooth transition
+    slider[0].appendChild(start);
 
-        setTimeout(function(){
-            function moveLeft(item){
-                var currentMargin = parseInt(item.style.marginLeft) ? parseInt(item.style.marginLeft) : 0;
-                var max = -1 * (imageWidth - 100);
-                var min = -50;
-                if(currentMargin < max){
+    //Sets the slider width to the sum of all the images, so they end up next to each other
+    var sliderWidth = imageWidth * images.length;
+    slider[0].style.width = sliderWidth + "px";
+
+//Animation
+setInterval(function() {
+        function moveLeft(item) {
+            var currentMargin = parseInt(item.style.marginLeft) ? parseInt(item.style.marginLeft) : 0;
+            var max = (-1 * (imageWidth - 100)) + currentMargin;
+
+            //Reset and cancel movement at the end of the last picture
+            if(-1 * (sliderWidth - imageWidth) >= (currentMargin)){item.style.marginLeft = 0 + "px"; clearInterval(moveImage);}
+            else{
+            if (currentMargin < max) {
                 item.style.marginLeft = currentMargin - 2 + "px";
-                }
-                else{
-                    item.style.marginLeft = currentMargin - 10 + "px";
-                }
-                if(currentMargin < (-1 * imageWidth)){
-                    clearInterval(moveImage);
-                }
             }
-            var moveImage = setInterval(function(){moveLeft(slider[0]);}, 1);}, 1000);
+            else {
+                item.style.marginLeft = currentMargin - 10 + "px";
+            }
 
+            if (currentMargin % (-1 * imageWidth) == 0) {
+                clearInterval(moveImage);
+            }}
+        }
+
+        var moveImage = setInterval(function () {moveLeft(slider[0]);}, 1);
+}, 2000);
