d053b642a1cd7715cb4406bd90f4ff214eb2ebb3
Mikaela Grundin <me@mikaelagrundin.com>
2015-05-28 10:06:52 +0200

    Slightly refined animation and pictures next to each other

--- js/slider.js
+++ js/slider.js
@@ -2,11 +2,22 @@ var slider = document.querySelectorAll('.img-slider'),
     images = document.querySelectorAll('.img-item'),
     imageWidth = images[0].clientWidth;
 
+    slider[0].style.width = imageWidth * images.length + "px";
 
-function moveLeft(item){
-    var currentMargin = parseInt(item.style.marginLeft) ? parseInt(item.style.marginLeft) : 0;
-    item.style.marginLeft = currentMargin - 1 + "px";
+        setTimeout(function(){
+            function moveLeft(item){
+                var currentMargin = parseInt(item.style.marginLeft) ? parseInt(item.style.marginLeft) : 0;
+                var max = -1 * (imageWidth - 100);
+                var min = -50;
+                if(currentMargin < max){
+                item.style.marginLeft = currentMargin - 2 + "px";
+                }
+                else{
+                    item.style.marginLeft = currentMargin - 10 + "px";
+                }
+                if(currentMargin < (-1 * imageWidth)){
+                    clearInterval(moveImage);
+                }
+            }
+            var moveImage = setInterval(function(){moveLeft(slider[0]);}, 1);}, 1000);
 
-}
-
-setInterval(function(){moveLeft(slider[0]);}, 10);
